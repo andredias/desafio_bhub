d523f4d33865b5639af32771e7248d0be9add5c8
André Felipe Dias <andre.dias@pronus.io>
2023-12-17 11:17:12 -0300

    regra para cálculo do preço total e teste do Manager

--- bhub/business_rules.py
+++ bhub/business_rules.py
@@ -11,7 +11,7 @@ class Cotacao(BaseModel):
     preco: Decimal
     regra_promocao: str | None = None
     unidade_medida: str | None = None
-    preco_por_unidade: Decimal | None = None
+    preco_por_unidade_medida: Decimal | None = None
     quantidade: float = 0
     preco_total: Decimal | None = None
 
@@ -31,6 +31,14 @@ class Manager:
             except ValueError as error:
                 logger.error(error)
 
+    def handle_many(self, cotacoes: list[Cotacao]) -> None:
+        for cotacao in cotacoes:
+            self.handle(cotacao)
+
+
+def calc_preco_total(cotacao: Cotacao) -> None:
+    cotacao.preco_total = cotacao.preco * Decimal(str(cotacao.quantidade))
+
 
 def calc_preco_por_unidade(cotacao: Cotacao) -> None:
     padrao = r'\s((?:\d+,)?\d+)(ml|l|g|kg)$'
@@ -45,7 +53,7 @@ def calc_preco_por_unidade(cotacao: Cotacao) -> None:
         fator = 1000
         unidade = unidade.replace('ml', 'l').replace('g', 'kg')
     cotacao.unidade_medida = unidade
-    cotacao.preco_por_unidade = cotacao.preco / Decimal(qtd) * fator
+    cotacao.preco_por_unidade_medida = cotacao.preco / Decimal(qtd) * fator
 
 
 def calc_leve_x_page_y(cotacao: Cotacao) -> None:
@@ -86,6 +94,7 @@ def calc_deconto_sobre_quantidade(cotacao: Cotacao) -> None:
 
 manager = Manager(
     [
+        calc_preco_total,  # deve ser o primeiro da lista
         calc_preco_por_unidade,
         calc_leve_x_page_y,
         calc_deconto_sobre_quantidade,
--- tests/test_business_rules.py
+++ tests/test_business_rules.py
@@ -2,43 +2,45 @@ from decimal import Decimal
 
 from bhub.business_rules import (
     Cotacao,
+    Manager,
     calc_deconto_sobre_quantidade,
     calc_leve_x_page_y,
     calc_preco_por_unidade,
+    calc_preco_total,
 )
 
 
 def test_calc_preco_por_unidade() -> None:
     cotacao = Cotacao(nome_produto='Batata Extra kg', preco=Decimal('2.99'))
     calc_preco_por_unidade(cotacao)
-    assert cotacao.preco_por_unidade is None
+    assert cotacao.preco_por_unidade_medida is None
     assert cotacao.unidade_medida is None
 
     cotacao.nome_produto = '150g Queijo Mussarela'
     calc_preco_por_unidade(cotacao)
     assert cotacao.unidade_medida is None
-    assert cotacao.preco_por_unidade is None
+    assert cotacao.preco_por_unidade_medida is None
 
     cotacao.nome_produto = 'Chocolate 80g'
     cotacao.preco = Decimal('8.0')
     calc_preco_por_unidade(cotacao)
     assert cotacao.unidade_medida == 'kg'
-    assert cotacao.preco_por_unidade == Decimal('100.0')
+    assert cotacao.preco_por_unidade_medida == Decimal('100.0')
 
     cotacao = Cotacao(nome_produto='Coca-Cola 2L', preco=Decimal('10.0'))
     calc_preco_por_unidade(cotacao)
     assert cotacao.unidade_medida == 'l'
-    assert cotacao.preco_por_unidade == Decimal('5.0')
+    assert cotacao.preco_por_unidade_medida == Decimal('5.0')
 
     cotacao = Cotacao(nome_produto='Iogurte 300ml', preco=Decimal('2.7'))
     calc_preco_por_unidade(cotacao)
     assert cotacao.unidade_medida == 'l'
-    assert cotacao.preco_por_unidade == Decimal('9.0')
+    assert cotacao.preco_por_unidade_medida == Decimal('9.0')
 
     cotacao = Cotacao(nome_produto='Serotonina 30mg', preco=Decimal('10.0'))
     calc_preco_por_unidade(cotacao)
     assert cotacao.unidade_medida is None
-    assert cotacao.preco_por_unidade is None
+    assert cotacao.preco_por_unidade_medida is None
 
 
 def test_calc_leve_x_page_y() -> None:
@@ -107,3 +109,81 @@ def test_calc_deconto_sobre_quantidade() -> None:
     )
     calc_deconto_sobre_quantidade(cotacao)
     assert cotacao.preco_total == Decimal('89.5')
+
+
+def test_calc_preco_total() -> None:
+    cotacao = Cotacao(nome_produto='fubá', preco=Decimal('10.0'), quantidade=0)
+    calc_preco_total(cotacao)
+    assert cotacao.preco_total == Decimal('0')
+
+    cotacao = Cotacao(nome_produto='fubá', preco=Decimal('5.99'), quantidade=2)
+    calc_preco_total(cotacao)
+    assert cotacao.preco_total == Decimal('11.98')
+
+    cotacao = Cotacao(nome_produto='fubá', preco=Decimal('1.5'), quantidade=0.75)
+    calc_preco_total(cotacao)
+    assert cotacao.preco_total == Decimal('1.125')
+
+
+def test_busines_rules_manager() -> None:
+    manager = Manager(
+        [
+            calc_preco_total,
+            calc_preco_por_unidade,
+            calc_leve_x_page_y,
+            calc_deconto_sobre_quantidade,
+        ]
+    )
+    cotacoes = [
+        Cotacao(
+            nome_produto='Cebola Roxa Kg',
+            preco=Decimal('6.0'),
+            quantidade=0.75,
+        ),
+        Cotacao(
+            nome_produto='fubá pacote 1,5Kg',
+            regra_promocao='Leve 3, Pague 2',
+            preco=Decimal('6.0'),
+            quantidade=3,
+        ),
+        Cotacao(
+            nome_produto='Arroz Tipo 1 5Kg',
+            preco=Decimal('25.0'),
+            regra_promocao='para 3 unidades ou mais, desconto de 15%',
+            quantidade=2,
+        ),
+        Cotacao(
+            nome_produto='Feijão Preto 1Kg',
+            preco=Decimal('5.99'),
+            regra_promocao='para 3 unidades ou mais, desconto de 15%',
+            quantidade=5,
+        ),
+    ]
+    resultados: list[dict] = [
+        {
+            'preco_por_unidade_medida': None,
+            'unidade_medida': None,
+            'preco_total': Decimal('4.5'),
+        },
+        {
+            'preco_por_unidade_medida': Decimal('4.0'),
+            'unidade_medida': 'kg',
+            'preco_total': Decimal('12.0'),
+        },
+        {
+            'preco_por_unidade_medida': Decimal('5.0'),
+            'unidade_medida': 'kg',
+            'preco_total': Decimal('50.0'),
+        },
+        {
+            'preco_por_unidade_medida': Decimal('5.99'),
+            'unidade_medida': 'kg',
+            'preco_total': Decimal('25.4575'),
+        },
+    ]
+
+    manager.handle_many(cotacoes)
+    for cotacao, resultado in zip(cotacoes, resultados, strict=True):
+        assert cotacao.preco_por_unidade_medida == resultado['preco_por_unidade_medida']
+        assert cotacao.unidade_medida == resultado['unidade_medida']
+        assert cotacao.preco_total == resultado['preco_total']
